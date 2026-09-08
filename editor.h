c22b32b451164783b87c402a2a7f6ad28f1cdb18
ADARSH PATIL <25020103175@reva.edu.in>
2026-09-08 23:33:10 +0530

    EDITOR.c

---
+++ editor.h
@@ -0,0 +1,14 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+#define MAX_LINES 100
+#define MAX_LENGTH 500
+
+extern char lines[MAX_LINES][MAX_LENGTH];
+extern int lineCount;
+
+/* Core functions */
+void insertLine(int position);
+void deleteLine(int position);
+void displayDocument(void);
+
